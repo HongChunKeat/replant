4bb5f31534db93df4947466bd37b5e0320eb0b6c
Eric Hong <erichong8442@gmail.com>
2024-03-15 19:12:17 +0800

    fix

--- plugin/development/config/route.php
+++ plugin/development/config/route.php
@@ -6,7 +6,7 @@ use plugin\development\app\controller as oxDev;
 Route::group("/development", function () {
     // test
     Route::group("/test", function () {
-        Route::get("/testing", [oxDev\test\Test::class, "testing"]);
+        // Route::get("/testing", [oxDev\test\Test::class, "testing"]);
         // Route::get("/testing2", [oxDev\test\Test::class, "testing2"]);
         // Route::get("/testing3", [oxDev\test\Test::class, "testing3"]);
         // Route::get("/cron", [app\crontab\tasks\reward\Reward::class, "handle"]);
